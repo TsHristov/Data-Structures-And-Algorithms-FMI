d4d2854be3a5c11bb02a9f050b0cdd190a757870
TsvetanHristov <tsvetanhristov@yahoo.com>
2015-10-25 21:51:58 +0200

    templates for DynamicArray added

--- Homework_No1/Homework_No1/DynamicArray.h
+++ Homework_No1/Homework_No1/DynamicArray.h
@@ -1,11 +1,12 @@
 #include<iostream>
 #pragma once
+template<class T>
 class DynamicArray
 {
 private:
 	size_t size;
 	size_t capacity;
-	int* phead;
+	T* phead;
 
 public:
 	DynamicArray();
@@ -14,12 +15,105 @@ public:
 	const DynamicArray& operator=(const DynamicArray&);
 
 public:
-	void insert(int);
+	void insert(T);
 	void resize();
-	int& operator[](int);
+	T& operator[](int);
 	void free();
 
 public:
 	void print() const;
 };
 
+template<class T>
+DynamicArray<T>::DynamicArray()
+{
+	size = 0;
+	capacity = 3;
+	phead = new T[capacity];
+	std::cout << "constructing..." << '\n';
+}
+
+template<class T>
+DynamicArray<T>::~DynamicArray()
+{
+	free();
+	std::cout << "destructing..." << this << '\n';
+}
+
+template<class T>
+DynamicArray<T>::DynamicArray(const DynamicArray& o){
+	phead = new T[o.capacity];
+	for (size_t i = 0; i <= o.size; ++i){
+		phead[i] = o.phead[i];
+	}
+	size = o.size;
+	capacity = o.capacity;
+	std::cout << "Copy constructor..." << '\n';
+}
+
+template<class T>
+const DynamicArray<T>& DynamicArray<T>::operator=(const DynamicArray& o){
+	if (this == &o){
+		return *this;
+	}
+	else{
+		free();
+		phead = new T[o.capacity];
+		for (size_t i = 0; i < o.capacity; ++i){
+			phead[i] = o.phead[i];
+		}
+		size = o.size;
+		capacity = o.capacity;
+	}
+	std::cout << "operator=" << '\n';
+	return *this;
+}
+
+template<class T>
+T& DynamicArray<T>::operator[](int index){
+	/*if (index < 0 || index > capacity - 1){
+	std::cout << "Index out of bounds!" << '\n';
+	exit(1);
+	}*/
+	std::cout << "operator[]" << '\n';
+	return phead[index];
+}
+
+template<class T>
+void DynamicArray<T>::insert(T element){
+	if (size == capacity)
+	{
+		std::cout << "enters the if clause..." << '\n';
+		this->resize();
+	}
+	phead[size++] = element;
+	std::cout << "insert.." << '\n';
+}
+
+template<class T>
+void DynamicArray<T>::resize(){
+	T* pToResized = new T[capacity * 2];
+	for (size_t i = 0; i < capacity; ++i){
+		pToResized[i] = phead[i];
+	}
+	delete[] phead;
+	capacity *= 2;
+	phead = pToResized;
+}
+
+template<class T>
+void DynamicArray<T>::free(){
+	if (phead){
+		delete[] phead;
+		phead = NULL;
+	}
+	size = 0;
+	capacity = 0;
+}
+
+template<class T>
+void DynamicArray<T>::print() const{
+	for (int i = 0; i < capacity; ++i){
+		std::cout << "[" << i << "]:" << phead[i] << '\n';
+	}
+}
--- Homework_No1/Homework_No1/main.cpp
+++ Homework_No1/Homework_No1/main.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
 #include "DynamicArray.h"
 int main(){
-	DynamicArray obj;
+	DynamicArray<int> obj;
 	obj.insert(1);
 	obj.insert(2);
 	obj.insert(3);
 	obj.insert(4);
+	obj.insert(5);
+	obj.insert(6);
+	obj.insert(7);
 	obj.print();
+
+	DynamicArray<int> other(obj);
+	other.print();
+	other.insert(9);
+	other.print();
+
 	return 0;
 }
