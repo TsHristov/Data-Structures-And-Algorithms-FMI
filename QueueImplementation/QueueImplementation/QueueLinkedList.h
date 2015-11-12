cbe24a3ad86e73789dcf0ed3bc0f229ce63a8440
TsvetanHristov <tsvetanhristov@yahoo.com>
2015-11-12 13:10:31 +0200

    added Peek() method and removed Enqueue and Dequeue definitions outside the class

--- QueueImplementation/QueueImplementation/QueueLinkedList.h
+++ QueueImplementation/QueueImplementation/QueueLinkedList.h
@@ -1,45 +1,63 @@
 #pragma once
 template<class T>
 class QueueLinkedList{
+
+private:
 	struct Node{
 		T data;
 		Node *next;
 		Node(const T& v, Node *n) :next(n), data(v){}
 		~Node(){
-			delete next;
+			if (!next){
+				delete next;
+			}
 		}
 	};
+
+private:
 	Node *head, *tail;
-	//size_t size;
+	size_t size;
+
 public:
-	QueueLinkedList(){
-		head = nullptr;
-		tail = nullptr;
-		//size = 0;
-	}
-	~QueueLinkedList(){ 
-		delete head; 
-	}
-	void Enqueue(const T& v){
-		Node *n = new Node(v, NULL);
-		tail->next = n;
-		tail = n;
-		//++size;
-		if (!head){
-			head = n;
-		}
-	}
-	T& Dequeue(){
-		T headData = head->data;
-		Node *newHead = head->next;
-		delete head;
-		head = newHead;
-		return headData;
-	}
-	/*bool IsEmpty(){
-		return size == 0;
-	}*/
-	bool IsFull(){
-		return head == tail;
-	}
+	QueueLinkedList():head(nullptr), tail(nullptr),size(0){}
+	~QueueLinkedList(){delete head;}
+	
+public:
+	void Enqueue(const T&);
+	void Dequeue();
+	T Peek() const;
+	size_t GetSize() const{ return this->size; } 
 };
+
+
+template<class T>
+void QueueLinkedList<T>::Enqueue(const T& value){
+	Node *newElem = new Node(value, nullptr);
+	if (!tail){
+		tail = newElem;
+	}
+	else{
+		tail->next = newElem;
+		tail = newElem;
+	}
+	if (!head){
+		head = newElem;
+	}
+	++size;
+	std::cout << newElem->data;
+}
+
+
+template<class T>
+void QueueLinkedList<T>::Dequeue(){
+	Node *removed = head;
+	head = removed->next;
+	delete removed;
+	--size;
+}
+
+
+template<class T>
+T QueueLinkedList<T>::Peek() const{
+	return head->data;
+}
--- QueueImplementation/QueueImplementation/main.cpp
+++ QueueImplementation/QueueImplementation/main.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
 #include "QueueLinkedList.h"
-int main(){
-	QueueLinkedList<int> obj;
-	obj.Enqueue(1);
-	obj.Enqueue(2);
-	obj.Enqueue(3);
-	obj.Enqueue(4);
 
-	int x = obj.Dequeue();
-	std::cout << x << '\n';
+int main(){
+	QueueLinkedList<char> obj;
+	
+	std::cout << "Enqueued: \n";
+	for (size_t i = 0; i < 20; ++i){
+		obj.Enqueue('a' + i);
+	}
+	std::cout << "\nDequeued: \n";
+
+	size_t i = obj.GetSize();
+	while (i)
+	{
+		std::cout << obj.Peek() << '\n';
+		obj.Dequeue();
+		--i;
+	}
 
 	return 0;
 }
