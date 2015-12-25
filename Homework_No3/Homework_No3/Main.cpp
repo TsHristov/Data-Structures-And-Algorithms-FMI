0b3e062e25e910ede30b283dcce8090e0fe93d0a
TsvetanHristov <tsvetanhristov@yahoo.com>
2015-12-25 11:55:18 +0200

    raix sort now works perfectly

--- Homework_No3/Homework_No3/Main.cpp
+++ Homework_No3/Homework_No3/Main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#define CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 using namespace std;
 
 //Get position of the letter in the array as index of it
@@ -34,16 +37,16 @@ int getMax(string names[], int n)
 }
 
 // A function to perform counting sort on the names based on the letter
-void countSort(string names[], int n, int step, int maxLength)
+void countSort(string names[], int N, int step)
 {
 	std::cout << "Pass No: " << step << endl;
 	
-	string output[5];
+	string* output = new string[N];
 	int i, count[26] = { 0 };
 	
 	// Store count of occurrences in count[]
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < N; i++)
 	{
 		if (names[i].size() >= step)
 		{
@@ -51,12 +54,9 @@ void countSort(string names[], int n, int step, int maxLength)
 			std::cout << " Name: " << names[i] << " | Letter: " << getLastChar(names[i], step) <<
 				" | Position: " << getPosition(getLastChar(names[i], step)) << endl;
 		}
-		else
+		else if (names[i].size() < step) // case step == maxLength
 		{
-			count[getPosition(getLastChar(names[i], names[i].size()))]++;
-			std::cout << " Name: " << names[i] << " | Letter: " << getLastChar(names[i],names[i].size()) <<
-				" | Position: " << getPosition(getLastChar(names[i], names[i].size())) << endl;
-
+			count[0]++;
 		}
 
 	}
@@ -68,50 +68,57 @@ void countSort(string names[], int n, int step, int maxLength)
 		count[i] = count[i - 1] + count[i];
 
 	// Build the output array
-	for (i = n - 1; i >= 0; i--)
+	for (i = N - 1; i >= 0; i--)
 	{
 		if (names[i].size() >= step)
 		{
 			output[count[getPosition(getLastChar(names[i], step))] - 1] = names[i];
 			count[getPosition(getLastChar(names[i], step))]--;
 		}
-		else
+		else if (names[i].size() < step) // case step == maxLength
 		{
-			output[count[getPosition(getLastChar(names[i], names[i].size()))] - 1] = names[i];
-			count[getPosition(getLastChar(names[i], names[i].size()))]--;
+			output[count[0] - 1] = names[i];
+			count[0]--;
 		}
 	}
 
 	// Copy the output array to arr[], so that arr[] now
 	// contains sorted names according to current letter
-	for (i = 0; i < n; i++)
+	for (i = 0; i < N; i++)
 	{
 		if (output[i] != "")
 		names[i] = output[i];
 	}
 	std::cout << " \n Result: ";
-	print(names, 5);
+	print(names, N);
 	std::cout << endl << endl;
+	delete []output;
 }
 
 
-void radixsort(string names[], int n)
+void radixsort(string names[], int N)
 {
 	// Find the maximum length of  word to know number of letters
-	int maxLength = getMax(names, 5);
+	int maxLength = getMax(names, N);
 
 	// Do counting sort for every letter.
 	// step - on the first step look at the last letter,on the second the letter before the last and so on...
 	int step;
 	for (step = 1; step <= maxLength; ++step)
-		countSort(names, 5, step, maxLength);
+		countSort(names, N, step);
 }
 
 int main()
 {
-	string names[] = { "ivan", "mitko" , "joro", "peshe" ,"tobi" };
-	radixsort(names, 5);
-	print(names, 5);
+	{
+		int N;
+		cout << "N=";
+		cin >> N;
+		string names[] = { "eli", "mama", "pesho", "maq" }; // { "ivan", "mitko", "joro", "peshe", "tobi" };
+		radixsort(names, N);
+		print(names, N);
+	}
+	_CrtDumpMemoryLeaks();
 
 	return 0;
 }
